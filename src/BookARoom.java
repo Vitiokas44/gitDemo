f922ff12d0d2fee34f68cf1fb0027afc6f1ee811
Vitiokas44 <68917862+Vitiokas44@users.noreply.github.com>
2020-08-01 15:02:14 -0500

    Update BookARoom.java

--- src/BookARoom.java
+++ src/BookARoom.java
@@ -2,5 +2,7 @@ public class BookARoom {
     public static void main(String[] args) {
         System.out.println("haha");
         System.out.println("hi");
+        //hi
+        //hello zdraste
     }
 }
