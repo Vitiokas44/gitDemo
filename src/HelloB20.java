b3a7e5e4017c77f435d1619925262b0fa1cef6ab
Vitiokas44 <68917862+Vitiokas44@users.noreply.github.com>
2020-08-01 15:31:38 -0500

    Update HelloB20.java

--- src/HelloB20.java
+++ src/HelloB20.java
@@ -5,9 +5,9 @@ public class HelloB20 {
         System.out.println("hi hi hi hi");
         System.out.println("hello");
         
-        System.out.println(""');
+        System.out.println("");
         System.out.println("hi");
-        System.out.println("jjjjjjjjjjjj);
+        System.out.println("jjjjjjjjjjjj");
         System.out.println("hhh");
         System.out.println("salut");
 
