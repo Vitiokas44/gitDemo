2fa7a7aa6e9ee85984bc4e2d62afecebc131a355
Vitiokas44 <russ25081989@gmail.com>
2020-08-01 11:30:18 -0500

    update HelloB20.java

--- src/HelloB20.java
+++ src/HelloB20.java
@@ -1,6 +1,11 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class HelloB20 {
     public static void main(String[] args) {
         System.out.println("hi hi hi hi");
+        System.out.println("hello");
+
+        System.out.println();
     }
 
 }
